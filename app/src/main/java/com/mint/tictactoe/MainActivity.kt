bb031c4c4197cc57d4b79e3a46482b04dadb79fb
hkapplemint <laitatwai@gmail.com>
2023-06-21 20:15:37 +0800

    Tic Tac Toe with minimax

--- app/src/main/java/com/mint/tictactoe/MainActivity.kt
+++ app/src/main/java/com/mint/tictactoe/MainActivity.kt
@@ -2,6 +2,7 @@ package com.mint.tictactoe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
@@ -14,23 +15,23 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val iv00: ImageView = findViewById(R.id.iv00)
-        iv00.tag = "iv00"
+        iv00.tag = "00"
         val iv01: ImageView = findViewById(R.id.iv01)
-        iv01.tag = "iv01"
+        iv01.tag = "01"
         val iv02: ImageView = findViewById(R.id.iv02)
-        iv02.tag = "iv02"
+        iv02.tag = "02"
         val iv10: ImageView = findViewById(R.id.iv10)
-        iv10.tag = "iv10"
+        iv10.tag = "10"
         val iv11: ImageView = findViewById(R.id.iv11)
-        iv11.tag = "iv11"
+        iv11.tag = "11"
         val iv12: ImageView = findViewById(R.id.iv12)
-        iv12.tag = "iv12"
+        iv12.tag = "12"
         val iv20: ImageView = findViewById(R.id.iv20)
-        iv20.tag = "iv20"
+        iv20.tag = "20"
         val iv21: ImageView = findViewById(R.id.iv21)
-        iv21.tag = "iv21"
+        iv21.tag = "21"
         val iv22: ImageView = findViewById(R.id.iv22)
-        iv22.tag = "iv22"
+        iv22.tag = "22"
 
         val btnRestart:Button = findViewById(R.id.btnRestart)
 
@@ -59,35 +60,44 @@ class MainActivity : AppCompatActivity() {
 
         for (imageView in ivList){
             imageView.setOnClickListener{
-                val imageViewY = imageView.tag.toString().substring(2,3).toInt()
-                val imageViewX = imageView.tag.toString().substring(3).toInt()
+                val imageViewY = imageView.tag.toString().substring(0,1).toInt()
+                val imageViewX = imageView.tag.toString().substring(1).toInt()
 
                 if(playerTurnIsCircle && !gameEnded){
                     if(boardArray[imageViewY][imageViewX] == "Empty"){
                         imageView.setImageResource(R.drawable.circle)
                         boardArray[imageViewY][imageViewX] = "Circle"
-                        checkWin()
-                        checkDraw()
+                        checkWinTwo()
+                        checkDrawTwo()
                         playerTurnIsCircle = false
                     }
                 } else if (!playerTurnIsCircle && !gameEnded) {
                     if(boardArray[imageViewY][imageViewX] == "Empty"){
                         imageView.setImageResource(R.drawable.cross)
                         boardArray[imageViewY][imageViewX] = "Cross"
-                        checkWin()
-                        checkDraw()
+                        checkWinTwo()
+                        checkDrawTwo()
                         playerTurnIsCircle = true
                     }
                 }
+
                 if(!gameEnded){
-//                    val botMove = getBestMove()
-//                    renderBoard()
+                    val handler = Handler()
+                    handler.postDelayed({
+                        val botMove = getBestMove()
+
+                        boardArray[botMove.first][botMove.second] = "Cross"
+                        renderBoard()
+
+                        checkWinTwo()
+                        checkDrawTwo()
+
+                        playerTurnIsCircle = true
+                    }, 500 )
+
 //                    val tv:TextView = findViewById(R.id.tv)
-//                    tv.text = botMove.toString() + gameEnded.toString()
-//                    boardArray[botMove.first][botMove.second] = "Cross"
-//                    checkWin()
-//                    checkDraw()
-//                    playerTurnIsCircle = true
+//                    tv.text = gameEnded.toString() + playerTurnIsCircle.toString()
+
                 }
 
             }
@@ -110,6 +120,86 @@ class MainActivity : AppCompatActivity() {
 
 
     fun checkWin():String {
+        val tv: TextView = findViewById(R.id.tv)
+        //Check horizontal
+        for (i in 0..2) {
+            if (boardArray[i].toSet().size == 1) {
+                if ("Circle" in boardArray[i]) {
+                    tv.text = "Circle Wins"
+//                    gameEnded = true
+                    return "Circle Wins"
+                } else if ("Cross" in boardArray[i]) {
+                    tv.text = "Cross Wins"
+//                    gameEnded = true
+                    return "Cross Wins"
+                }
+            }
+        }
+
+        //Check vertical
+        val firstColumn = mutableListOf<String>()
+        val secondColumn = mutableListOf<String>()
+        val thirdColumn = mutableListOf<String>()
+        for (i in 0..2) {
+            firstColumn.add(boardArray[i][0])
+        }
+        for (i in 0..2) {
+            secondColumn.add(boardArray[i][1])
+        }
+        for (i in 0..2) {
+            thirdColumn.add(boardArray[i][2])
+        }
+        val columnList = listOf(firstColumn, secondColumn, thirdColumn)
+        for (column in columnList){
+            if(column.toSet().size == 1){
+                if("Circle" in column){
+                    tv.text = "Circle Wins"
+//                    gameEnded = true
+                    return "Circle Wins"
+                } else if ("Cross" in column){
+                    tv.text = "Cross Wins"
+//                    gameEnded = true
+                    return "Cross Wins"
+                }
+            }
+        }
+
+        //Check Diagonals
+        val firstDiagonal = mutableListOf<String>()
+        for (i in 0..2){
+            for (j in 0..2){
+                if (i==j){
+                    firstDiagonal.add(boardArray[i][j])
+                }
+            }
+        }
+        val secondDiagonal = mutableListOf<String>()
+        for (i in 0..2){
+            for (j in 0..2){
+                if(i+j == 2){
+                    secondDiagonal.add(boardArray[i][j])
+                }
+            }
+        }
+        val diagonalList = listOf(firstDiagonal, secondDiagonal)
+        for (diagonal in diagonalList){
+            if(diagonal.toSet().size == 1){
+                if("Circle" in diagonal){
+                    tv.text = "Circle Wins"
+//                    gameEnded = true
+                    return "Circle Wins"
+                } else if ("Cross" in diagonal){
+                    tv.text = "Cross Wins"
+//                    gameEnded = true
+                    return "Cross Wins"
+                }
+            }
+        }
+        return ""
+    }
+
+
+    fun checkWinTwo():String {
         val tv: TextView = findViewById(R.id.tv)
         //Check horizontal
         for (i in 0..2) {
@@ -237,11 +327,28 @@ class MainActivity : AppCompatActivity() {
         }
         if(emptyFound==0){
             tv.text = "Draw"
+//            gameEnded = true
+            return true
+        }
+        return false
+    }
+
+    fun checkDrawTwo():Boolean{
+        val tv: TextView = findViewById(R.id.tv)
+        var emptyFound = 0
+        for (row in boardArray){
+            if("Empty" in row){
+                emptyFound += 1
+            }
+        }
+        if(emptyFound==0){
+            tv.text = "Draw"
             gameEnded = true
             return true
         }
         return false
     }
+
 
     fun getAvailalbeMoves(): List<Pair<Int, Int>>{
         val availableMoves = mutableListOf<Pair<Int, Int>>()
